5e97a928f7cfa55ac29a3cfb2afe996c53325b37
zenaide-s <zenaidesilvasouto@gamil.com>
2025-03-01 19:36:32 -0300

    Segunda atualização do contador

--- TesteGit.java
+++ TesteGit.java
@@ -1,6 +1,6 @@
 public class TesteGit{
 	public static void main(String[] args){
-		for(int a = 0; a <= 1000; a++){
+		for(int a = 0; a <= 5000; a++){
 			System.out.println("Contador = "+a);
 		}	
 	}
